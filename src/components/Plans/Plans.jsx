683dde4127c8a4d8bd6496f427bcbaf0452cb7f7
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2024-05-09 16:10:02 -0300

    Feat: implementando planos

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import './Plans.css';
+import { plansData } from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
 
 const Plans = () => {
     return (
-        <div>Plans</div>
+        <div className='plans-container'>
+            {/* header */}
+            <div className="plans-header" style={{ gap: '2rem' }}>
+                <span className='stroke-text'>READY TO START</span>
+                <span>OUR JOURNEY</span>
+                <span className='stroke-text'>NOW WITHUS</span>
+            </div>
+            {/* Plans cards */}
+            <div className="plans">
+                {plansData.map((plans, i) =>
+                    <div className="plan" key={i}>
+                        {plans.icon}
+                        <span>{plans.name}</span>
+                        <span>$ {plans.price}</span>
+
+                        <div className="features">
+                            {plans.features.map((feature, i) => 
+                            <div className="feature">
+                                <img src={whiteTick} alt="" />
+                                <span key={i}>{feature}</span>
+                            </div>
+                            )}
+                        </div>
+                        <div>
+                            <span>See more benefits -></span>
+                        </div>
+                        <button className="btn">Join now</button> 
+                    </div>
+                )}
+            </div>
+
+        </div>
     )
 }
 
