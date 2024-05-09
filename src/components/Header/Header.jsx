a3e6188472b2e22d8c5220fcc33c3a090d6cc31d
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2024-05-09 12:51:02 -0300

    Feat: Implementado lado esquerdo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Header.css';
-
+import Logo from '../../assets/logo.png'
 const Header = () => {
     return(
-        <div className="header"></div>
+        <div className="header">
+
+            <img src={Logo} className='logo' />
+
+            <ul className='header-menu'>
+                <li>Home</li>
+                <li>Programs</li>
+                <li>Why us</li>
+                <li>Plans</li>
+                <li>Testamunials</li>
+            </ul>
+        </div>
     )
 }
 
