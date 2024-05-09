b902d58c09a72846739f390d4cbabc480b8ee152
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2024-05-09 17:46:55 -0300

    Feat: implemetnado Join e emailjs

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Join.css';
+import emailjs from '@emailjs/browser';
+
 
 const Join = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLETE_ID', form.current, 'YOUR_USER_ID')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text)
+            });
+    };
+
     return (
-        <div>Join</div>
+        <div className="join" id="join-us">
+            <div className="left-j">
+                <hr />
+                <div>
+                    <span className='stroke-text'>READY TO</span>
+                    <span>LEVEL UP</span>
+                </div>
+                <div>
+                    <span>YOUR</span>
+                    <span className='stroke-text'>WITH US?</span>
+                </div>
+            </div>
+            <div className="right-j">
+                <form ref={form} class="email-container" onSubmit={sendEmail}>
+                    <input type="email" name='user_email' placeholder='Enter your Email Address here...' />
+                    <button type='submit' class="btn btn-j">Join now</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
