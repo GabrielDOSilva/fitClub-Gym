51a8327ad8085f508398fd1ef37489a277ae4cef
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2024-05-09 14:24:24 -0300

    Feat: Implementado Programas

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import './Programs.css'
+import './Programs.css';
+import { programsData } from '../../data/programsData';
+import RighArrow from '../../assets/rightArrow.png';
 
 const Programs = () => {
     return (
-        <div>Progrmas</div>
+        <div className="Programs" id="programs">
+            {/* header */}
+            <div className="programs-header">
+                <span className='stroke-text'>Explore our</span>
+                <span>Progrmas</span>
+                <span className='stroke-text'>To shape you</span>
+            </div>
+
+            <div className="programs-categorires">
+                {programsData.map((program) =>(
+                    <div className="category">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className="join-now">
+                            Join Now
+                            <span><img src={RighArrow} alt="" /></span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+
     )
 }
 
